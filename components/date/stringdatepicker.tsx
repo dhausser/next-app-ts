02ba555a0d421491c78540c7485e381757cc2960
Davy Hausser <davy.hausser@gmail.com>
2021-07-24 20:45:53 +0800

    Form experiment with normalize

---
+++ components/date/stringdatepicker.tsx
@@ -0,0 +1,28 @@
+import dayjs from 'dayjs'
+import utc from 'dayjs/plugin/utc'
+import timezone from 'dayjs/plugin/timezone'
+import { Form } from 'antd'
+import { PickerProps } from 'antd/es/date-picker/generatePicker'
+import { DatePicker } from '.'
+
+dayjs.extend(utc)
+dayjs.extend(timezone)
+
+dayjs.tz.setDefault('America/New_York')
+
+// String to date
+export const stringToDate = (date?: string) => (date ? dayjs(date) : null)
+
+// interface Props extends Omit<PickerProps<dayjs.Dayjs>, 'value'> {
+//   value?: string
+// }
+
+function StringDatePicker(
+  props: PickerProps<dayjs.Dayjs>
+): React.FunctionComponentElement<PickerProps<dayjs.Dayjs>> {
+  const { value } = props
+  const date = typeof value === 'string' ? stringToDate(value) : value
+  return <DatePicker {...props} value={date} />
+}
+
+export { dayjs, utc, timezone, StringDatePicker }
--- components/product.tsx
+++ components/product.tsx
@@ -1,7 +1,9 @@
 import { Form, Input, Checkbox, Button, Select, Space } from 'antd'
 import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons'
-import { RangePicker } from './date'
+import { StringDatePicker, dayjs } from './date/stringdatepicker'
+import { DatePicker } from './date'
 import { findCommonDateIndexes } from '../utils/datetime'
+import { Dayjs } from 'dayjs'
 
 const areas = [
   { label: 'Beijing', value: 'Beijing' },
@@ -25,6 +27,11 @@ const dates = [
   '2019-04-04T16:00:00.000Z',
   '2019-04-04T16:00:00.000Z',
 ]
+
+// Function to convert a date to a string
+const dateToString = (date: Dayjs): string => {
+  return date.format('YYYY-MM-DD')
+}
 
 const formItemLayout = {
   labelCol: {
@@ -56,8 +63,6 @@ interface FormType {
 }
 
 export default function Product() {
-  const [form] = Form.useForm<FormType>()
-
   const onFinish = (values: any) => {
     console.log('Success:', values)
   }
@@ -66,19 +71,21 @@ export default function Product() {
     console.log('Failed:', errorInfo)
   }
 
+  const [form] = Form.useForm()
+
   const commonDateIndexes = findCommonDateIndexes(dates)
 
   console.log(commonDateIndexes)
 
   return (
     <Form
-      form={form}
-      name="dynamic_form_nest_item"
       {...formItemLayout}
+      form={form}
       initialValues={{
         username: 'Luke',
         password: '123456',
         remember: true,
+        // date: '2018-04-04T16:00:00.000Z',
         sights: [
           { area: areas[0].value, sight: sights.Beijing[0] },
           { area: areas[1].value, sight: sights.Shanghai[0] },
@@ -87,155 +94,161 @@ export default function Product() {
       onFinish={onFinish}
       onFinishFailed={onFinishFailed}
     >
-      <Form.Item
-        label="Username"
-        name="username"
-        rules={[{ required: true, message: 'Please input your username!' }]}
-      >
-        <Input style={{ width: '40%' }} />
-      </Form.Item>
+      {({ setFieldsValue, resetFields }) => (
+        <>
+          <Form.Item
+            label="Username"
+            name="username"
+            normalize={(value) => value.toUpperCase()}
+            rules={[{ required: true, message: 'Please input your username!' }]}
+          >
+            <Input style={{ width: '40%' }} />
+          </Form.Item>
 
-      <Form.Item
-        name="password"
-        label="Password"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your password!',
-          },
-        ]}
-      >
-        <Input.Password style={{ width: '40%' }} />
-      </Form.Item>
+          <Form.Item
+            name="password"
+            label="Password"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your password!',
+              },
+            ]}
+          >
+            <Input.Password style={{ width: '40%' }} />
+          </Form.Item>
 
-      <Form.Item
-        label="Date"
-        name="date"
-        rules={[
-          {
-            required: true,
-            message: 'Please input a date!',
-          },
-        ]}
-      >
-        <RangePicker showTime allowClear allowEmpty={[false, true]} />
-      </Form.Item>
+          <Form.Item
+            label="Date"
+            name="date"
+            tooltip="Timezone: Europe/Warsaw"
+            normalize={(value) => {
+              console.log('normalize', value)
+              return value
+            }}
+          >
+            <StringDatePicker />
+          </Form.Item>
 
-      <Form.Item
-        name="remember"
-        valuePropName="checked"
-        wrapperCol={{ offset: 4 }}
-      >
-        <Checkbox>Remember me</Checkbox>
-      </Form.Item>
+          <Form.Item
+            name="remember"
+            valuePropName="checked"
+            wrapperCol={{ offset: 4 }}
+          >
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
 
-      <Form.List
-        name="sights"
-        rules={[
-          {
-            validator: async (_, names) => {
-              if (!names || names.length < 2) {
-                return Promise.reject(new Error('At least 2 sights'))
-              }
-            },
-          },
-        ]}
-      >
-        {(fields, { add, remove }, { errors }) => (
-          <>
-            {fields.map(({ key, name, fieldKey, ...restField }, index) => (
-              <Form.Item
-                {...(index === 0 ? formItemLayout : formItemLayoutWithOutLabel)}
-                label={index === 0 ? 'Sight' : ''}
-                required={false}
-                key={key}
-              >
-                <Space
-                  key={key}
-                  style={{ display: 'flex', marginBottom: 8 }}
-                  align="baseline"
-                >
+          <Form.List
+            name="sights"
+            rules={[
+              {
+                validator: async (_, names) => {
+                  if (!names || names.length < 2) {
+                    return Promise.reject(new Error('At least 2 sights'))
+                  }
+                },
+              },
+            ]}
+          >
+            {(fields, { add, remove }, { errors }) => (
+              <>
+                {fields.map(({ key, name, fieldKey, ...restField }, index) => (
                   <Form.Item
-                    {...restField}
-                    name={[name, 'area']}
-                    fieldKey={[fieldKey, 'area']}
-                    noStyle
+                    {...(index === 0
+                      ? formItemLayout
+                      : formItemLayoutWithOutLabel)}
+                    label={index === 0 ? 'Sight' : ''}
+                    required={false}
+                    key={key}
                   >
-                    <Select
-                      options={areas}
-                      onChange={(value) =>
-                        form.setFieldsValue({
-                          sights: form
-                            .getFieldValue('sights')
-                            .map((sight: Sight, sightIndex: number) => ({
-                              ...sight,
-                              sight:
-                                sightIndex === index
-                                  ? sights[`${value}`][0]
-                                  : sight.sight,
-                            })),
-                        })
-                      }
-                      style={{ width: 185 }}
-                    />
-                  </Form.Item>
+                    <Space
+                      key={key}
+                      style={{ display: 'flex', marginBottom: 8 }}
+                      align="baseline"
+                    >
+                      <Form.Item
+                        {...restField}
+                        name={[name, 'area']}
+                        fieldKey={[fieldKey, 'area']}
+                        noStyle
+                      >
+                        <Select
+                          options={areas}
+                          onChange={(value) =>
+                            setFieldsValue({
+                              sights: form
+                                .getFieldValue('sights')
+                                .map((sight: Sight, sightIndex: number) => ({
+                                  ...sight,
+                                  sight:
+                                    sightIndex === index
+                                      ? sights[`${value}`][0]
+                                      : sight.sight,
+                                })),
+                            })
+                          }
+                          style={{ width: 185 }}
+                        />
+                      </Form.Item>
 
-                  <Form.Item
-                    {...restField}
-                    name={[name, 'sight']}
-                    fieldKey={[fieldKey, 'sight']}
-                    validateTrigger={['onChange', 'onBlur']}
-                    noStyle
+                      <Form.Item
+                        {...restField}
+                        name={[name, 'sight']}
+                        fieldKey={[fieldKey, 'sight']}
+                        validateTrigger={['onChange', 'onBlur']}
+                        noStyle
+                      >
+                        <Select
+                          options={sights[
+                            form.getFieldValue(['sights', index, 'area'])
+                          ].map((sight) => ({
+                            label: sight,
+                            value: sight,
+                          }))}
+                          style={{ width: 185 }}
+                        />
+                      </Form.Item>
+
+                      {fields.length > 1 ? (
+                        <MinusCircleOutlined
+                          className="dynamic-delete-button"
+                          onClick={() => remove(name)}
+                        />
+                      ) : null}
+                    </Space>
+                  </Form.Item>
+                ))}
+                <Form.Item {...formItemLayoutWithOutLabel}>
+                  <Button
+                    type="dashed"
+                    onClick={() => {
+                      const [{ value: area }] = areas
+                      const [sight] = sights[area]
+                      add({ area, sight })
+                    }}
+                    style={{ width: '40%', marginTop: '20px' }}
+                    icon={<PlusOutlined />}
                   >
-                    <Select
-                      options={sights[
-                        form.getFieldValue(['sights', index, 'area'])
-                      ].map((sight) => ({
-                        label: sight,
-                        value: sight,
-                      }))}
-                      style={{ width: 185 }}
-                    />
-                  </Form.Item>
+                    Add field
+                  </Button>
+                  <Form.ErrorList errors={errors} />
+                </Form.Item>
+              </>
+            )}
+          </Form.List>
 
-                  {fields.length > 1 ? (
-                    <MinusCircleOutlined
-                      className="dynamic-delete-button"
-                      onClick={() => remove(name)}
-                    />
-                  ) : null}
-                </Space>
-              </Form.Item>
-            ))}
-            <Form.Item {...formItemLayoutWithOutLabel}>
-              <Button
-                type="dashed"
-                onClick={() => {
-                  const [{ value: area }] = areas
-                  const [sight] = sights[area]
-                  add({ area, sight })
-                }}
-                style={{ width: '40%', marginTop: '20px' }}
-                icon={<PlusOutlined />}
-              >
-                Add field
+          <Form.Item wrapperCol={{ offset: 4 }}>
+            <Space>
+              <Button type="primary" htmlType="submit">
+                Submit
               </Button>
-              <Form.ErrorList errors={errors} />
-            </Form.Item>
-          </>
-        )}
-      </Form.List>
-
-      <Form.Item wrapperCol={{ offset: 4 }}>
-        <Space>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-          <Button type="ghost" onClick={() => form.resetFields()}>
-            Reset
-          </Button>
-        </Space>
-      </Form.Item>
+              <Button type="ghost" onClick={() => resetFields()}>
+                Reset
+              </Button>
+            </Space>
+          </Form.Item>
+        </>
+      )}
     </Form>
   )
 }
